0717093bc7cc52cc8b0087fc7fbf870ebdf549f3
vasialendiuk <73075660+vasialendiuk@users.noreply.github.com>
2020-12-14 13:41:53 +0200

    Add files via upload

---
+++ lab3/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { CardComponent } from './card/card.component';
+import { FormComponent } from './form/form.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CardComponent,
+    FormComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
